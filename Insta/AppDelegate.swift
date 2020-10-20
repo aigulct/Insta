d26de071821a8c0566491e4209014b8b7a1fbd83
Aigul <aigulct@gmail.com>
2020-10-19 23:05:31 -0700

    User can sign up and log in

--- Insta/AppDelegate.swift
+++ Insta/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let parseConfig = ParseClientConfiguration {
+            $0.applicationId = "j1e0QzA7njOXl9fXegtyoMKHzgSKgqHdXkVwcEtL"
+            $0.clientKey = "weJvaKYd71oUA1ZWfD4hetjWiwvuTSzgL2h6sURa"
+            $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
         return true
     }
 
